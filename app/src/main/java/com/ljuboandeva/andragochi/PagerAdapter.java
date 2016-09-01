7691d5eea1f77770154635b34b26dc8abfef691c
Ljuben Vassilev <ljuben.vassilev@gmail.com>
2016-09-01 21:31:13 +0300

    Activities work
    Shop and Settings

---
+++ app/src/main/java/com/ljuboandeva/andragochi/PagerAdapter.java
@@ -0,0 +1,45 @@
+package com.ljuboandeva.andragochi;
+
+/**
+ * Created by ljubo on 9/1/2016.
+ */
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.ljuboandeva.andragochi.fragments.FoodFragment;
+import com.ljuboandeva.andragochi.fragments.MedsFragment;
+import com.ljuboandeva.andragochi.fragments.ToysFragment;
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+    int mNumOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                FoodFragment foodFR = new FoodFragment();
+                return foodFR;
+            case 1:
+                ToysFragment toysFR = new ToysFragment();
+                return toysFR;
+            case 2:
+                MedsFragment medsFR = new MedsFragment();
+                return medsFR;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}

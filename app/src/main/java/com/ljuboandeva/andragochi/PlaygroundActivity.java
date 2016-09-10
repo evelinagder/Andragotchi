99677fb5a17bbb4adbf252b195a90b427c37e22c
Ljuben Vassilev <ljuben.vassilev@gmail.com>
2016-09-11 00:47:28 +0300

    Navigation fully complete within all activities
    Settings, shop, inventory accessible from both home and playground
    Name changer in Settings fully working

--- app/src/main/java/com/ljuboandeva/andragochi/PlaygroundActivity.java
+++ app/src/main/java/com/ljuboandeva/andragochi/PlaygroundActivity.java
@@ -1,19 +1,36 @@
 package com.ljuboandeva.andragochi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.RelativeLayout;
+
+import com.ljuboandeva.andragochi.model.players.User;
+import com.ljuboandeva.andragochi.model.players.UsersManager;
 
 public class PlaygroundActivity extends AppCompatActivity {
 
     String outChoice;
+    Button home;
+    Button shop;
+    ImageButton inventory;
+    ImageButton settings;
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playground);
-
         RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.layout_playground);
+        home = (Button) findViewById(R.id.button_home);
+        user=(User) getIntent().getExtras().get("loggedUser");
+        shop=(Button)findViewById(R.id.button_shop_playground);
+        inventory= (ImageButton) findViewById(R.id.button_inventory_playground);
+        settings= (ImageButton) findViewById(R.id.button_settings_playground);
+
         outChoice = getIntent().getStringExtra("outChoice");
         switch (outChoice){
             case "park":
@@ -29,5 +46,42 @@ public class PlaygroundActivity extends AppCompatActivity {
 
 
 
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PlaygroundActivity.this,HomeActivity.class);
+                intent.putExtra("loggedUser", UsersManager.getInstance(PlaygroundActivity.this).getUser(user.getUsername()));
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        shop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(PlaygroundActivity.this, ShopActivity.class);
+                intent.putExtra("loggedUser", UsersManager.getInstance(PlaygroundActivity.this).getUser(user.getUsername()));
+                startActivity(intent);
+            }
+        });
+
+        inventory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(PlaygroundActivity.this, InventoryActivity.class);
+                intent.putExtra("loggedUser", UsersManager.getInstance(PlaygroundActivity.this).getUser(user.getUsername()));
+                startActivity(intent);
+            }
+        });
+
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(PlaygroundActivity.this, SettingsActivity.class);
+                intent.putExtra("loggedUser", UsersManager.getInstance(PlaygroundActivity.this).getUser(user.getUsername()));
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/ljuboandeva/andragochi/SettingsActivity.java
+++ app/src/main/java/com/ljuboandeva/andragochi/SettingsActivity.java
@@ -7,18 +7,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.ljuboandeva.andragochi.model.pet.Pet;
+import com.ljuboandeva.andragochi.model.players.User;
+import com.ljuboandeva.andragochi.model.players.UsersManager;
+
 public class SettingsActivity extends AppCompatActivity {
+
+    Button button_logout;
+    Button changeName;
+    EditText nameET;
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
-        Button button_logout;
-
+        user=(User) getIntent().getExtras().get("loggedUser");
+        changeName = (Button) findViewById(R.id.button_changeName);
+        nameET = (EditText) findViewById(R.id.name_settings);
         button_logout = (Button) findViewById(R.id.button_logout);
+
+        changeName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String newName=nameET.getText().toString();
+                if(newName.isEmpty()){
+                    nameET.setError("Pet name is empty!");
+                    nameET.requestFocus();
+                    return;
+                }
+                Pet temp = user.getPet();
+                temp.setName(newName);
+                UsersManager.getInstance(SettingsActivity.this).setUserPet(SettingsActivity.this,user, temp);
+                Toast.makeText(SettingsActivity.this,"Name changed",Toast.LENGTH_LONG).show();
+            }
+        });
 
         button_logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,5 +61,13 @@ public class SettingsActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent= new Intent(SettingsActivity.this,HomeActivity.class);
+        intent.putExtra("loggedUser", user);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
     }
 }
